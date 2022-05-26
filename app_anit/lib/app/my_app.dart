b063e671081e8b3a4062b4a003506d1c32705a30
ZavbakGit <ag@799000.ru>
2022-05-26 20:43:25 +1000

    add

--- app_anit/lib/app/my_app.dart
+++ app_anit/lib/app/my_app.dart
@@ -1,7 +1,40 @@
 import 'package:app_anit/navigation/my_router_test.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 import '../navigation/my_router.dart';
+
+MaterialColor generateMaterialColor(Color color) {
+  return MaterialColor(color.value, {
+    50: tintColor(color, 0.5),
+    100: tintColor(color, 0.4),
+    200: tintColor(color, 0.3),
+    300: tintColor(color, 0.2),
+    400: tintColor(color, 0.1),
+    500: tintColor(color, 0),
+    600: tintColor(color, -0.1),
+    700: tintColor(color, -0.2),
+    800: tintColor(color, -0.3),
+    900: tintColor(color, -0.4),
+  });
+}
+
+int tintValue(int value, double factor) =>
+    max(0, min((value + ((255 - value) * factor)).round(), 255));
+
+Color tintColor(Color color, double factor) => Color.fromRGBO(
+    tintValue(color.red, factor),
+    tintValue(color.green, factor),
+    tintValue(color.blue, factor),
+    1);
+
+final ThemeData theme = ThemeData(
+  primarySwatch: generateMaterialColor(Color.fromARGB(255, 216, 111, 111)),
+);
+
+class Palette {
+  static const Color primary = Color(0xFF2F4D7D);
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,9 +49,10 @@ class MyApp extends StatelessWidget {
       routerDelegate: router.routerDelegate,
       debugShowCheckedModeBanner: false,
       title: 'Anit APP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      theme: theme,
     );
   }
 }
