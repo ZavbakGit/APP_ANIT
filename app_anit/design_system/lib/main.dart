475f1d9b4041d426befaeddbcae12ef9d90c184f
ZavbakGit <ag@799000.ru>
2022-07-29 08:57:42 +1000

    add

--- app_anit/design_system/lib/main.dart
+++ app_anit/design_system/lib/main.dart
@@ -31,17 +31,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
-        children: [
-          const DsText.headingOne('Design System'),
-          verticalSpaceSmall,
-          const Divider(),
-          verticalSpaceSmall,
-          ...buttonWidgets,
-          ...textWidgets,
-          ...inputFields,
-        ],
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+          children: [
+            const DsText.headingOne('Design System'),
+            verticalSpaceSmall,
+            const Divider(),
+            verticalSpaceSmall,
+            ...buttonWidgets,
+            ...textWidgets,
+            ...inputFields,
+          ],
+        ),
       ),
     );
   }
