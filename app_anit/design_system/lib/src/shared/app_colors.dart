1037a96ca1f56b5bb5085b2b64e5532f86ef72d6
ZavbakGit <ag@799000.ru>
2022-09-01 21:52:57 +1000

    clear

--- app_anit/design_system/lib/src/shared/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kcPrimaryColor = Color(0xff22A45D);
-const Color kcMediumGreyColor = Color(0xff868686);
-const Color kcLightGreyColor = Color(0xffe5e5e5);
-const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
