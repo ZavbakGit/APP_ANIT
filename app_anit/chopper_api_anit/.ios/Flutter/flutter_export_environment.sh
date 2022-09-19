b1e54edf58c92402d90fa3d09f7bd92e9a53f4fc
ZavbakGit <ag@799000.ru>
2022-09-19 17:30:44 +1000

    add

--- app_anit/chopper_api_anit/.ios/Flutter/flutter_export_environment.sh
+++ app_anit/chopper_api_anit/.ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Project\flutter\APP_ANIT\app_anit\chopper_api_anit"
+export "FLUTTER_APPLICATION_PATH=E:\Project\Flutter\APP_ANIT\app_anit\chopper_api_anit"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
